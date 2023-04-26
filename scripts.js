c4292fa41c252fe0bb11ce389cc3cc3d16cebb66
eliasraffaelli0 <eliasraffaelli@gmail.com>
2023-04-26 11:17:06 -0300

    player object created

--- scripts.js
+++ scripts.js
@@ -1,12 +1,29 @@
-const player = ()=>{
-    
+const player = (team)=>{
+    this.team = team;
+
+    const getTeam = () => {
+        return team;
+    };
+
+    return { getTeam };
 };
 
 const gameBoard = (() => {
-    const board = ["", "", "", "", "", "", "", "", "", ];
+    const board = ["X", "X", "X", "X", "0", "0", "X", "0", "F", ];
+    
+    const fields = document.querySelectorAll('.field');
+
+    for(let i=0; board.length; i++){
+        fields[i].innerText = board[i];
+    }
+
+})();
+
+const displayController = (()=>{
 
 })();
 
 const gameController = (()=>{
 
 })();
+
