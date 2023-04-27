ad6cb7ab7e4f7e909a082a9852f1cf5f7e173287
eliasraffaelli0 <eliasraffaelli@gmail.com>
2023-04-27 11:05:08 -0300

    add a move by cliking done

--- scripts.js
+++ scripts.js
@@ -9,17 +9,36 @@ const player = (team)=>{
 };
 
 const gameBoard = (() => {
-    const board = ["X", "X", "X", "X", "0", "0", "X", "0", "F", ];
-    
-    const fields = document.querySelectorAll('.field');
+    const board = ["", "", "", "", "", "", "", "", ""];
 
-    for(let i=0; board.length; i++){
-        fields[i].innerText = board[i];
+    const insertField = (pos, team) => {
+        if(pos> board.length) return;
+        board[pos]  = team;
+    };
+
+    const getField = (pos) => {
+        return board[pos];
     }
 
+    return {insertField, getField};
 })();
 
 const displayController = (()=>{
+    const fields = document.querySelectorAll('.field');
+
+    //adds the event to each cell so when it's clicked adds the player's move to the board array
+    fields.forEach(field => field.addEventListener('click', ()=>{
+        const team = field.innerText = 'k';
+        const pos = field.getAttribute('data-index')
+        
+        gameBoard.insertField(pos,team)
+        updateBoard()
+    }));
+
+    //update every field with the board array value using the data-index as the position
+    const updateBoard = () => {
+        fields.forEach(field => field.innerText = gameBoard.getField(field.getAttribute('data-index')))
+    }
 
 })();
 
