698b16b93f1a4f7ad69f4583ed02a17959026700
eliasraffaelli0 <eliasraffaelli@gmail.com>
2023-05-02 10:15:51 -0300

    players turn logic done

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const player = (team)=>{
+const player = (team) => {
     this.team = team;
 
     const getTeam = () => {
@@ -23,26 +23,39 @@ const gameBoard = (() => {
     return {insertField, getField};
 })();
 
-const displayController = (()=>{
+const displayController = (() => {
     const fields = document.querySelectorAll('.field');
 
     //adds the event to each cell so when it's clicked adds the player's move to the board array
     fields.forEach(field => field.addEventListener('click', ()=>{
-        const team = field.innerText = 'k';
-        const pos = field.getAttribute('data-index')
+        if(field.innerText !== "") return;
         
-        gameBoard.insertField(pos,team)
+        const pos = field.getAttribute('data-index')
+        gameController.playRound(pos)
         updateBoard()
     }));
 
     //update every field with the board array value using the data-index as the position
     const updateBoard = () => {
-        fields.forEach(field => field.innerText = gameBoard.getField(field.getAttribute('data-index')))
+        fields.forEach(field => field.innerText = gameBoard.getField(field.getAttribute('data-index')));
     }
 
 })();
 
-const gameController = (()=>{
+const gameController = (() => {
+    const playerX = player("X");
+    const playerO = player("O");
+    let round = 1;
 
+    const playRound = (pos) => {
+        gameBoard.insertField(pos, getPlayerTeam());
+        round++;
+    }
+
+    const getPlayerTeam = () => {
+        return round % 2 === 1 ? playerX.getTeam() : playerO.getTeam();
+    }
+
+    return { playRound }
 })();
 
